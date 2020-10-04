340c5427449c79811040541d5ba4d406266a6b3a
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-10-04 14:50:02 +0300

    fix test.api

--- _test_/api.test.js
+++ _test_/api.test.js
@@ -1,25 +1,22 @@
 'use strict';
 
-const jwt = require('jsonwebtoken');
-const  route  = require('../lib/router/routes');
+const { server } = require('../lib/server');
 const supergoose = require('@code-fellows/supergoose');
 
-const mockRequest = supergoose(route);
+const mockRequest = supergoose(server);
 
 describe('Auth Router', () => {
-    it('can sign up', async () => {
-      const userData = {
-        username:'Ahmad',
-        password: '123',
-        fullname:'AHMADK',
-        email: 'aHMADK@jo.com',
-        skill: 'web-DEV'
-      };
-      const results = await mockRequest.post('/signup').send(userData);
-      let user = results.body;
-    //   expect(userData[username]).toEqual(user[username]);
-    });
-
-// });
+  it('can sign up', async () => {
+    const userData = {
+      username: 'Ahmad',
+      password: '123',
+      fullname: 'AHMADK',
+      email: 'aHMADK@jo.com',
+      skill: 'web-DEV',
+    };
+    const results = await mockRequest
+      .post('/signup')
+      .send(userData);
+    expect(results.body['username']).toEqual(userData['username']);
+  });
 });
-
