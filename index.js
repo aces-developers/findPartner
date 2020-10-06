246dd0f6fd70c15daf8ee877523d7781cb683d2a
HidayaSyam <hid.sy1910@gmail.com>
2020-10-06 15:08:29 +0300

    fixed

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 'use strict';
-
 const mongoose = require('mongoose');
 const serverModule =require('./lib/server');
-
 require('dotenv').config();
-
 const mongooseOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 };
-let MONGOOSE_URL = process.env.MONGOOSE_URL;
+let MONGOOSE_URL = process.env.MONGO_DB_URL;
 mongoose.connect(MONGOOSE_URL, mongooseOptions)
   .then(()=>console.log('mongodb connected'))
   .catch(err=> console.log(err));
-
-serverModule.start();
+  serverModule.start();
